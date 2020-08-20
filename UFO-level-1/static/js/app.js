63f60dac062d4f5e355bcf0e8fd95628312e9935
KEFAN LI <1350-kefan.li99@users.noreply.columbia.bootcampcontent.com>
2020-08-19 23:45:17 -0400

    New

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -30,24 +30,31 @@ var form = d3.select("#form");
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
+// function to run filter
 function runEnter() {
 
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
+    // set variable for input value in the box
     var inputValue = inputElement.property("value");
+    // take the table 
     var table = document.getElementById("ufo-table");
     var tr = table.getElementsByTagName("tr");
 
-
+    // for loop through table 
     for (i = 0; i < tr.length; i++) {
+        // table data = loop through each table row in index 0
         td = tr[i].getElementsByTagName("td")[0];
         if (td) {
+            // the value match the html and actual text
             txtValue = td.textContent || td.innerText;
+            // to auto uppercase all input and search each index from 0 onward to avoid -1 index failed search
             if (txtValue.toUpperCase().indexOf(inputValue) > -1) {
+                // display style in property to show/hide results
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
-            }
-        }
-    }
-}
+            };
+        };
+    };
+};
